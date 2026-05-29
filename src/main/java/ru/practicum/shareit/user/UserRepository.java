8eb8e9e087ed9848759c4339d33724f87ba1b063
Goose <lnpscoop@gmail.com>
2026-05-29 06:14:36 +0500

    Подключена база данных, добавлена возможность бронировать вещи

--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -7,6 +7,8 @@ import java.util.*;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByEmail(String email);
+
     boolean existsByEmail(String email);
 }
