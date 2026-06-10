b14c5f7c7a86a3afc3bf2d6d39584d897ef4f24d
Goose <lnpscoop@gmail.com>
2026-06-11 01:43:36 +0500

    Дописаны тесты2

--- server/src/test/java/ru/practicum/shareit/controller/ItemControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/controller/ItemControllerTest.java
@@ -7,10 +7,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import ru.practicum.shareit.Comment.dto.CommentDto;
 import ru.practicum.shareit.item.ItemController;
 import ru.practicum.shareit.item.ItemService;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.dto.ItemWithBookingsDto;
+
 
 import java.util.Collections;
 
--- server/src/test/java/ru/practicum/shareit/controller/ItemRequestControllerTest.java
+++ server/src/test/java/ru/practicum/shareit/controller/ItemRequestControllerTest.java
@@ -11,7 +11,9 @@ import ru.practicum.shareit.request.ItemRequestController;
 import ru.practicum.shareit.request.ItemRequestService;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
 
+
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.*;
@@ -97,34 +99,6 @@ class ItemRequestControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(1))
                 .andExpect(jsonPath("$.description").value("Need a drill"));
-    }
-
-    @Test
-    void createRequest_ShouldReturnCreatedRequest() throws Exception {
-        ItemRequestDto inputDto = new ItemRequestDto();
-        inputDto.setDescription("Need a drill");
-
-        ItemRequestDto outputDto = new ItemRequestDto();
-        outputDto.setId(1L);
-        outputDto.setDescription("Need a drill");
-
-        when(requestService.createRequest(eq(1L), any(ItemRequestDto.class))).thenReturn(outputDto);
-
-        mockMvc.perform(post("/requests")
-                        .header("X-Sharer-User-Id", 1)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(inputDto)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(1));
-    }
-
-    @Test
-    void getOwnRequests_ShouldReturnList() throws Exception {
-        when(requestService.getOwnRequests(1L)).thenReturn(Collections.emptyList());
-
-        mockMvc.perform(get("/requests")
-                        .header("X-Sharer-User-Id", 1))
-                .andExpect(status().isOk());
     }
 
     @Test
