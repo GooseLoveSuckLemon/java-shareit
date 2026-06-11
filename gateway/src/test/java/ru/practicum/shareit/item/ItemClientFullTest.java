9d8ac24244a6e38cfa705a7be93dd5f1367849e9
Goose <lnpscoop@gmail.com>
2026-06-12 00:09:06 +0500

    ТестNTcn1

--- gateway/src/test/java/ru/practicum/shareit/item/ItemClientFullTest.java
+++ gateway/src/test/java/ru/practicum/shareit/item/ItemClientFullTest.java
@@ -2,14 +2,10 @@ package ru.practicum.shareit.item;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-import ru.practicum.shareit.item.dto.ItemDto;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 class ItemClientFullTest {
 
@@ -24,7 +20,6 @@ class ItemClientFullTest {
         RestTemplate restTemplate = mock(RestTemplate.class);
         ItemClient client = new ItemClient("http://localhost:8080",
                 new RestTemplateBuilder().requestFactory(() -> null));
-        // Тест на создание клиента
         assertThat(client).isNotNull();
     }
 }
