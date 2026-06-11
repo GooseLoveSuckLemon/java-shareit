99379391c0ff8c7cb1cac95ccf470a0642d40e7d
Goose <lnpscoop@gmail.com>
2026-06-12 00:44:32 +0500

    OXHKO

--- server/src/test/java/ru/practicum/shareit/service/BookingServiceImplCoverageTest.java
+++ server/src/test/java/ru/practicum/shareit/service/BookingServiceImplCoverageTest.java
@@ -15,7 +15,6 @@ import ru.practicum.shareit.booking.dto.BookingDto;
 import ru.practicum.shareit.booking.dto.BookingRequestDto;
 import ru.practicum.shareit.booking.dto.BookingState;
 import ru.practicum.shareit.exception.BadRequestException;
-import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.item.ItemRepository;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.User;
@@ -83,7 +82,6 @@ class BookingServiceImplCoverageTest {
 
     @Test
     void createBooking_WhenDatesEqual_ShouldThrowException() {
-        // ⚠️ НЕТ МОКОВ! Валидация дат происходит первой
         requestDto.setStart(LocalDateTime.now().plusDays(1));
         requestDto.setEnd(LocalDateTime.now().plusDays(1));
 
@@ -101,7 +99,7 @@ class BookingServiceImplCoverageTest {
                 .isInstanceOf(BadRequestException.class)
                 .hasMessageContaining("не может быть в прошлом");
     }
-    
+
     @Test
     void getBookingsByBooker_WithEmptyState_ShouldReturnEmpty() {
         when(userRepository.findById(2L)).thenReturn(Optional.of(booker));
